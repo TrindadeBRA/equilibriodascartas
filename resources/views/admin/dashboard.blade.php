8cb15a93f235f5e146e3f3f3e927dca4fcce7a9d
Lucas Trindade <trindadebra@gmail.com>
2023-01-28 22:00:18 -0300

    Ajustes layout dashboard

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br" >
+    <head>
+        <meta charset="UTF-8">
+        <title>Dashboard</title>
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.8.10/tailwind.min.css'><link rel="stylesheet" href="./style.css">
+        {{-- <link rel="stylesheet" href=" {{ @asset('css/style-basic.css') }} "> --}}
+    </head>
+    <body>
+        <div class="flex w-screen h-screen text-gray-400 bg-gray-900">
+
+            @include('admin.components.sidebar')
+
+            @include('admin.components.dropdown')
+
+            @include('admin.components.content')
+
+        </div>
+    
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home.index');
 });
+
+Route::get('/admin', function () {
+    return view('admin.dashboard');
+});
+
+Route::get('/admin/novo-jogo', function () {
+    return view('admin.dashboard');
+});
